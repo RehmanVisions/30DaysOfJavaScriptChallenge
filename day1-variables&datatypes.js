e5d735fa1d4f7ea265451286ecd980bb8ce4dbff
RehmanVisions <rehmansvision@gmail.com>
2024-07-20 04:50:56 +0000

    Day1-Variables&Datatypes

---
+++ day1-variables&datatypes.js
@@ -0,0 +1,28 @@
+// Activity 1: Variable Declaration
+
+// Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+var assignNumber = 2;
+
+// Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+let assignString = "String";
+
+// Activity 2: Constant Declaration
+
+// Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+const assignBoolean = true
+
+// Activity 3: Data Types
+
+// Task 4: Create varibles of different data types (number, string, boolean, object, array) and log each variable's type using the typeof operator.
+
+let number = 2;
+let string = "string"
+let boolean = false;
+let obj {
+    name = "Object"
+    dataTypeIn = "JavaScript"
+}
+let array = 
+
+console.table([assignNumber, assignString, assignBoolean])
+console.table([typeof assignNumber, typeof assignString, typeof assignBoolean])
