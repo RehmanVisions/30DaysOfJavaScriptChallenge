6b6cc8b7daa929b54545e2b8f0a39fe82ae11b01
RehmanVisions <rehmansvision@gmail.com>
2024-07-25 07:23:41 +0000

    day2-operators

---
+++ day2-operators.js
@@ -0,0 +1,74 @@
+// Activity 1: Arithmetic Operations
+
+// Task 1: Write a program to add two numbers and log the result to the console.
+let addition = 2 + 2;
+// console.log(addition);
+
+// Task 2: Write a program to subtract two numbers and log the result to the console.
+let subtract = 2 - 3;
+// console.log(subtract);
+
+// Task 3: Write a program to multiply two numbers and log the result to the console.
+let multiply = 2*3;
+// console.log(multiply);
+
+// Task 4: Write a program to divide two numbers and log the result to the console.
+let divide = 25 / 5;
+// console.log(divide);
+
+// Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+let remainder = 25 % 5;
+// console.log(remainder);
+
+// Activity 2: Assignment Operators
+
+// Task 6: Use the += operator to add a number to a variable and log the result to the console.
+let a = 5;
+a += 5;
+// console.log(a);
+
+// Task 7: Use the -= operator to subtract a number from a variable and log the result to the console.
+let b = 5;
+b -= 5;
+// console.log(a);
+
+// Activity 3: Comparison Operators
+
+// Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+let isGreater = 2 > 3;
+// console.log(compare1);
+let isLess = 2 < 3;
+// console.log(compare2);
+
+// Task 9: Write a program to compare two numbers using >= and <= and log the result to the console.
+let greaterOrEqual = 2 >= 3;
+// console.log(compare3);
+let lessOrEqual = 2 <= 3;
+// console.log(compare4);
+
+// Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+let Equal = "2"==2;
+// console.log(compare5);
+let strictEqual = "2"===2;
+// console.log(compare6);
+
+// Activity 4: Logical Operators
+
+// Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
+let combineCondi1 = ("5"==5) && ("5"==="5")
+// console.log(combineCondi1);
+
+// Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
+let combineCondi2 = ("5"==5) || ("5"==="5")
+// console.log(combineCondi2);
+
+// Task 13: Write a program that uses the ! operator to negate a condition and log the result to the console.
+let notTrue = !(5 > 1);
+// console.log(notTrue);
+
+// Activity 5: Ternary Operator
+
+// Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+let num = -5;
+let result = (num > 0)? 'positive' : 'negative';
+// console.log(result);
