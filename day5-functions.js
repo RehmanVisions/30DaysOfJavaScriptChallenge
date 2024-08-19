aff15f0b5d2460cb1fc253db67b9ec87750ba7cb
RehmanVisions <rehmansvision@gmail.com>
2024-08-19 06:48:21 +0000

    day5-functions

--- day5-functions.js
+++ day5-functions.js
@@ -12,8 +12,8 @@ function evenOrOdd(number){
     }
 }
 
-evenOrOdd(11)
-evenOrOdd(10)
+// evenOrOdd(11)
+// evenOrOdd(10)
 
 // Task2 : Write a function to calculate the square of a number and return the result.
 function squareOfNumber(number){
@@ -21,7 +21,7 @@ function squareOfNumber(number){
 }
 
 let resultoftask2 = squareOfNumber(5)
-console.log(resultoftask2);
+// console.log(resultoftask2);
  
 // Activity 2: Function Expression
 
@@ -34,8 +34,8 @@ function maxNum(a, b){
     }
 }
 
-console.log(maxNum(10, 20));
-console.log(maxNum(30, 20));
+// console.log(maxNum(10, 20));
+// console.log(maxNum(30, 20));
 
 
 // Task4 : Write a Function Expression to concatenate two strings and returns the result.
@@ -43,37 +43,37 @@ const concate = function(string1, string2) {
     return string1 + string2;
 }
 let resultOfTask4 = concate("Chai", "Code");
-console.log(resultOfTask4);
+// console.log(resultOfTask4);
 
 // Activity 3: Arrow Functions
 
 // Task5 : Write an Arrow function to calculate sum of two numbers and return the result
 const sum = (a, b) => a + b;
 let resultOfTask5 = sum(10, 4)
-console.log(resultOfTask5);
+// console.log(resultOfTask5);
 
 // Task 6 : Write an Arrow fuction to check if a string contains a specific character and return a boolean value.
 
 const containsCharacter = (str, char) => str.includes(char);
 let resultOfTask6 = containsCharacter("chai", "a" )
-console.log(resultOfTask6);
-console.log(typeof resultOfTask6);
+// console.log(resultOfTask6);
+// console.log(typeof resultOfTask6);
 
 // Activity 4: Fuction Parameters and default value
 
 // Task 7 : Write a function that takes two parameters and returns their product. Provide a default value for the second parameter.
 const twoParameters = (a, b = 2) => a * b;
 let resultOfTask7 = twoParameters(5, 3)
-console.log(resultOfTask7);
+// console.log(resultOfTask7);
 let resultOfTask72 = twoParameters(5)
-console.log(resultOfTask72);
+// console.log(resultOfTask72);
 
 // Task 8 : Write a function that takes a person's name and age and return a greeting message. provide a default value for the age.
 const yourNameAge = (name, age = "unknown") => `Hello, ${name}! your age is ${age}`
 let message = yourNameAge ("Hitech", 18);
-console.log(message);
+// console.log(message);
 let message2 = yourNameAge ("Hitech");
-console.log(message2);
+// console.log(message2);
 
 // Activity 5: Higher-Order Fuction
 
@@ -89,3 +89,21 @@ fnNum(repeatfn, 3)
 
 
 // Task 10 : Write a higher-order function that takes Two functions and a value, applies the first function to the value, and then applies the second function to the result.
+
+function HOFunctions(func1, func2, value) {
+    const result1 = func1(value);
+    const result2 = func2(result1)
+    return result2;
+}
+
+function triples(x) {
+    return x * 3;
+}
+
+function addTwo(x){
+    return x + 2
+}
+const value = 5;
+const result = HOFunctions(triples, addTwo, value);
+
+console.log(result);
